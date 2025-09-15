d4d225b2b8534914716d76299a526b0b80b09aca
rakhi9494-git <rakesh.kalidindi24@gmail.com>
2025-09-15 15:11:46 +1000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,11 @@
 variable "aws_region" {
-  default = "us-east-1"
+  default = "ap-southeast-2"
 }
 
 variable "ami_id" {
-  # Amazon Linux 2 AMI (for us-east-1) - change if using another region
-  default = "ami-0c02fb55956c7d316"
+  default = "ami-020e2d6e7640876e9"
 }
 
 variable "instance_type" {
-  default = "t2.micro"
+  default = "t3.micro"
 }
