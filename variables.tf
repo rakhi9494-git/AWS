2f84c89a12f330db194f64cdc204e58c86c9534e
rakhi9494-git <rakesh.kalidindi24@gmail.com>
2025-09-15 15:13:22 +1000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,3 +9,8 @@ variable "ami_id" {
 variable "instance_type" {
   default = "t3.micro"
 }
+
+variable "key_name" {
+  description = "Name of the existing AWS key pair to use for SSH"
+  default     = "my-key-pair"  # Replace with your actual key pair name
+}
